6e4070841b2ee1cc52322ca85f339eb72e6937e9
yousabbbbb <yousabmenissy@gmail.com>
2024-08-21 13:41:50 +0300

    Changed validation.s

--- lib/validation.s
+++ lib/validation.s
@@ -1,51 +1,51 @@
 .global validation
 .type validation, @function
 validation:
-    pushq %rbp
-    movq %rsp, %rbp
-    pushq %rbx
-    xor %rax, %rax
+	pushq %rbp
+	movq  %rsp, %rbp
+	pushq %rbx
+	pushq %rsi
+
+    xor  %rax, %rax
+	movq %rdi, %rsi
+	movq %rcx, %rcx
 
 sectionV0:
-    movq %rdi, %rsi
     lodsb
 
     cmpb   $0, %al
-	je 	   exit_valid
-
+	je 	   exit_invalid
     cmpb $'.', %al
-    jne .LLV0
-    movq $1, %rbx
-    je sectionV1
-
-.LLV0:
-    cmpb $'0', %al
-	jb exit_invalid
-
+    cmove %rcx, %rbx
+	cmpb $'-', %al
+	je   .LPV0
+	cmpb $'0', %al
+	jb   exit_invalid
 	cmpb $'9', %al
-	ja exit_invalid
+	ja   exit_invalid
+
+    jmp .LPV0
 
 sectionV1:
+	cmpq %rcx, %rbx
+	je   exit_invalid
+	movq %rcx, %rbx
+
 .LPV0:
     lodsb
 	cmpb   $0, %al
 	je 	   exit_valid
 
+    cmpb $'-', %al
+	je   exit_invalid
     cmpb $'.', %al
-    jne .LLV1
-    cmpq $1, %rbx
-    je  exit_invalid
-    movq $1, %rbx
-    jmp .LPV0
-
-.LLV1:
-	cmpb $'0', %al
-	jb exit_invalid
-
+	je   sectionV0
+  	cmpb $'0', %al
+	jb   exit_invalid
 	cmpb $'9', %al
-	ja exit_invalid
+	ja   exit_invalid
 
-    jmp .LPV0
+	jmp .LPV0
 
 exit_invalid:
     movq $-1, %rax
